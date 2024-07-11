7e11971643ab4584e48f04c8a9b921e0cfc2fed3
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-07-11 21:11:59 +0600

    module name changed using `go mod edit -module` commands

--- gobasic/go.mod
+++ gobasic/go.mod
@@ -1,3 +1,3 @@
-module gobasic
+module github.com/mujibulhaquetanim/gobasic
 
 go 1.22.5
--- gobasic/main.go
+++ gobasic/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
     "fmt"
-    "gobasic/struc" // Adjust relative path as per your GOPATH setup
+    "github.com/mujibulhaquetanim/gobasic/struc"
+    "github.com/mujibulhaquetanim/gobasic/datastructures"
 )
 
 func main() {
     p1 := struc.Person{Name: "Millie", Age: 20, Hobby: []string{"acting", "taking photos"}}
-
     fmt.Println(p1)
+
+    datastructures.StringMethods()
+
 }
