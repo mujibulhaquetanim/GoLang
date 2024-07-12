3e016c6be766eb8b558064f34e6606bc92682b27
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-07-12 23:30:19 +0600

    constructor with default values, method, interface & pointer added

---
+++ gobasic/struct/Struct.go
@@ -0,0 +1,48 @@
+package main
+
+import "fmt"
+
+type PersonMethods interface {
+	haveBirthDay()
+}
+
+type Person struct {
+	Name       string
+	Age        int
+	Hobby      []string
+	InLoveWith string
+}
+
+// constructor
+func NewPerson(name string, age int, hobby []string, inLoveWith string) *Person {
+
+	if name == "" {
+		name = "Emma Watson"
+	}
+
+	if len(hobby) == 0 {
+		hobby = []string{"acting", "taking photos"}
+	}
+
+	if inLoveWith == "" {
+		inLoveWith = "Mujibul Haque Tanim"
+	}
+	return &Person{
+		Name:       name,
+		Age:        age,
+		Hobby:      hobby,
+		InLoveWith: inLoveWith,
+	}
+}
+
+// method
+func (p *Person) haveBirthDay() {
+	p.Age++
+}
+
+func main() {
+	p1 := NewPerson("Millie bobby brown", 20, []string{}, "")
+
+	p1.haveBirthDay()
+	fmt.Println(p1)
+}
