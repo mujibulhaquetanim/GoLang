9867134ffe858a2f93ae5a353a9bdf25b5be1425
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-07-12 23:28:06 +0600

    struct file now executing in its package & string datastructure added

--- gobasic/main.go
+++ gobasic/main.go
@@ -1,16 +1,12 @@
 package main
 
 import (
-    "fmt"
-    "github.com/mujibulhaquetanim/gobasic/struc"
+    // "fmt"
     "github.com/mujibulhaquetanim/gobasic/datastructures"
     "github.com/mujibulhaquetanim/gobasic/concurrency"
 )
 
 func main() {
-    p1 := struc.Person{Name: "Millie", Age: 20, Hobby: []string{"acting", "taking photos"}}
-    fmt.Println(p1)
-    
     datastructures.StringMethods()
 
     concurrency.ConcurParrel()
