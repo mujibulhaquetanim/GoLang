fccaeaad0767cebb80caed4d1c30eb5efa2b871a
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-07-14 22:10:59 +0600

    mutax added using pointer to stop race condition for accessing score values.

--- gobasic/concurrency/mutax/mutax.go
+++ gobasic/concurrency/mutax/mutax.go
@@ -1,4 +1,43 @@
 package main
-import "fmt"
 
-func main() {fmt.Println("Hello, World!")}
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+
+	wg := &sync.WaitGroup{}
+	mut := &sync.Mutex{}
+
+	var score = []int{0}
+
+	wg.Add(3)
+	go func(wg *sync.WaitGroup, m *sync.Mutex) {
+		defer wg.Done()
+		m.Lock()
+		score = append(score, 1)
+		m.Unlock()
+		fmt.Println("one appended")
+	}(wg, mut)
+
+	go func(wg *sync.WaitGroup, m *sync.Mutex) {
+		defer wg.Done()
+		m.Lock()
+		score = append(score, 2)
+		m.Unlock()
+		fmt.Println("two appended")
+	}(wg, mut)
+
+	go func(wg *sync.WaitGroup, m *sync.Mutex) {
+		defer wg.Done()
+		m.Lock()
+		score = append(score, 3)
+		m.Unlock()
+		fmt.Println("three appended")
+	}(wg, mut)
+
+	wg.Wait()
+
+	fmt.Println(score)
+}
