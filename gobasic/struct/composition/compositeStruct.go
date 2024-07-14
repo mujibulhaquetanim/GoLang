c727ec29a8e351421d8c2b8b260488b021926e04
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-07-14 23:50:50 +0600

    composition struct value initialized using name arguments

--- gobasic/struct/composition/compositeStruct.go
+++ gobasic/struct/composition/compositeStruct.go
@@ -20,10 +20,16 @@ type PersonInfo struct {
 func main() {
 	// p1 := PersonInfo{"Millie bobby brown", 20, Address{"London", "England", "UK"}}
 	// p1 := PersonInfo{} //by default the value of string is " ", int is 0, so the output will be: { 0 {  }}
-	p1:= new (PersonInfo) //new returns a pointer, so we can use *p1 to access the value (dereferencing)
+	p1 := new(PersonInfo) //new returns a pointer, so we can use *p1 to access the value (dereferencing)
 	p1.Name = "Millie bobby brown"
 	p1.Age = 20
-	p1.Address = Address{"London", "England", "UK"}
+	//p1.Address = Address{"London", "England", "UK"}
+	p1.Address = Address{
+		City:    "London",
+		State:   "England",
+		Country: "UK",
+	}
 	fmt.Println(*p1)
 	fmt.Println(p1.Address.City) //London //accessing the nested struct
+	fmt.Print(p1.Address)
 }
