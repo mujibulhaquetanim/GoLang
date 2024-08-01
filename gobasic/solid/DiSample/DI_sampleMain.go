be99ec5cf9c112a32d499dfeabd06db49f1492fb
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-08-01 23:52:39 +0600

    simple example of di added

--- gobasic/solid/DiSample/DI_sampleMain.go
+++ gobasic/solid/DiSample/DI_sampleMain.go
@@ -3,10 +3,40 @@ package main
 import "fmt"
 
 type Vehicle interface {
-	seats() int
+	start()
+	stop()
+}
+
+type Car struct{}
+
+func (c Car) start(){
+	fmt.Println("The car has Started")
+}
+
+func (c Car) stop(){
+	fmt.Println("The car has stopped")
+}
+
+type MotorBike struct{}
+
+func (c MotorBike) start(){
+	fmt.Println("The MotorBike has Started")
+}
+
+func (c MotorBike) stop(){
+	fmt.Println("The MotorBike has stopped")
+}
+
+func operateVehicle(v Vehicle){
+	v.start()
+	v.stop()
 }
 
 func main(){
-
-	fmt.Println("Hello")
+	// myCar:= Car{}
+	myBike:= MotorBike{}
+	//without inserting hardcode code, we can inject any instance that meets vehicle contract.
+	//my car instance has been injected to the normal function that takes an Vehicle instance
+	// operateVehicle(myCar)
+	operateVehicle(myBike)
 }
