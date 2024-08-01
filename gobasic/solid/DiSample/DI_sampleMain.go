7484fc087c933843d5ff0072849764a56785e2fd
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-08-01 23:59:57 +0600

    va

--- gobasic/solid/DiSample/DI_sampleMain.go
+++ gobasic/solid/DiSample/DI_sampleMain.go
@@ -7,34 +7,38 @@ type Vehicle interface {
 	stop()
 }
 
-type Car struct{}
+type Car struct {
+	wheelNum int
+}
 
-func (c Car) start(){
+func (c Car) start() {
 	fmt.Println("The car has Started")
 }
 
-func (c Car) stop(){
+func (c Car) stop() {
 	fmt.Println("The car has stopped")
 }
 
-type MotorBike struct{}
-
-func (c MotorBike) start(){
-	fmt.Println("The MotorBike has Started")
+type MotorBike struct {
+	wheelNum int
 }
 
-func (c MotorBike) stop(){
-	fmt.Println("The MotorBike has stopped")
+func (m MotorBike) start() {
+	fmt.Printf("The MotorBike has Started with %d", m.wheelNum)
 }
 
-func operateVehicle(v Vehicle){
+func (m MotorBike) stop() {
+	fmt.Printf("\nThe MotorBike has stopped")
+}
+
+func operateVehicle(v Vehicle) {
 	v.start()
 	v.stop()
 }
 
-func main(){
+func main() {
 	// myCar:= Car{}
-	myBike:= MotorBike{}
+	myBike := MotorBike{wheelNum: 2}
 	//without inserting hardcode code, we can inject any instance that meets vehicle contract.
 	//my car instance has been injected to the normal function that takes an Vehicle instance
 	// operateVehicle(myCar)
