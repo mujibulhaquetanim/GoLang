d8e02f68f15141ab35e4cdf539ffd69a01ae1468
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-08-08 23:13:45 +0600

    string enum added

--- gobasic/Types/enums/enum_main.go
+++ gobasic/Types/enums/enum_main.go
@@ -4,22 +4,36 @@ import "fmt"
 
 //there is no type called enum in golang, but we can implement it using constants and types
 //enum is a type that can have only one value
-type enum int
+type OrderStatus int
 
 const (
-	Received enum = iota //iota is a counter that starts from 0 and increments by 1 for each constant in the enum type
+	Received OrderStatus = iota //iota is a counter that starts from 0 and increments by 1 for each constant in the enum type
 	Confirmed
 	Prepared
 	Dispatched
 	Shipped
 )
 
+type ProductType string
 
+const (
+	Book     ProductType = "book"
+	Toy      ProductType = "toy"
+	Pen                  = "pen" //we can write without the type
+	Notebook             = "notebook"
+)
 
-func changeOrderStatus(status enum) {
+func printProductType(productType ProductType) {
+	fmt.Println("product type: ", productType)
+}
+
+func changeOrderStatus(status OrderStatus) {
 	fmt.Println("order status: ", status)
 }
 
 func main() {
 	changeOrderStatus(Confirmed) // 1
+
+	printProductType(Pen) // pen
+
 }
