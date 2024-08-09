00cd8660f07f50a87856c0d3f91937eeea09b9c8
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-08-09 23:58:28 +0600

    type assertion using interface & struct

--- gobasic/Types/typeAssertions/Assertion_main.go
+++ gobasic/Types/typeAssertions/Assertion_main.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+//interface
+type Foer interface {
+	Foo()
+}
+type MyStruct struct{}
+
+func (f *MyStruct) Foo() {}
+func (b *MyStruct) Bar() {}
+
 func main() {
 	// as interface can be assigned to any type. using type assertion to check the type.
 	var a interface{} = 1
@@ -14,7 +23,11 @@ func main() {
 
 	//if else, type assertion: if the type assertion fails, the program will panic. if the type assertion succeeds, the program will continue. in the below example: if e is not an int, the program will panic.
 	var e interface{} = 1
-	if i, ok := e.(int); ok {
-		fmt.Println(i)
+	if onlyString, ok := e.(string); ok {
+		fmt.Println("e is a string", onlyString)
+	}
+	var f Foer = &MyStruct{}
+	if onlyMyStruct, ok := f.(interface{ Bar() }); ok {
+		onlyMyStruct.Bar()
 	}
 }
