02bcdcc8c9ba5934b25292dc215f5bc048340958
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-08-15 21:50:48 +0600

    used done channel with select to stop infinitely generating data.

--- gobasic/concurrency/done/done_main.go
+++ gobasic/concurrency/done/done_main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+func doWork(done chan bool) {
+	for {
+		select {
+		case <-done:
+			return
+		default:
+			fmt.Println("Working...")
+		}
+	}
+}
 
 func main() {
-	fmt.Println("done example")
+	done := make(chan bool)
+
+	go doWork(done)
+
+	time.Sleep(time.Second * 3)
+
+	fmt.Println("Stopping...\ndone")
+	close(done)
 }
