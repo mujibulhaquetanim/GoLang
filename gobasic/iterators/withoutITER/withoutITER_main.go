56ebac0a79f460a851db8349257d009bb47565ba
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-08-18 23:58:59 +0600

    iterator created with new fun(func()bool) signature syntax

--- gobasic/iterators/withoutITER/withoutITER_main.go
+++ gobasic/iterators/withoutITER/withoutITER_main.go
@@ -1,6 +1,23 @@
 package main
 
-// import "fmt"
+import "fmt"
+
+// CountDown iterator function takes in a value and returns a func that takes in another func with a signature of `func(int) bool`
+func CountDown(v int) func(func(int) bool) {
+
+	return func(yield func(int) bool) {
+		for i := v; i > 0; i-- {
+			// we then start a for loop that iterates
+			if !yield(i) {
+				// we then return and finish our iterations
+				return
+			}
+		}
+	}
+}
 
 func main() {
+	for x := range CountDown(5) {
+		fmt.Println(x)
+	}
 }
