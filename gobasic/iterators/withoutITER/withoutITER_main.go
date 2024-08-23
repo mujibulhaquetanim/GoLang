65e5c5f2f827899453ec6737be87d58c4456a9ca
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-08-23 23:51:04 +0600

    more complex example of iter which accept two parameter added using struct.

--- gobasic/iterators/withoutITER/withoutITER_main.go
+++ gobasic/iterators/withoutITER/withoutITER_main.go
@@ -16,8 +16,34 @@ func CountDown(v int) func(func(int) bool) {
 	}
 }
 
+//more complex example:
+type Employee struct {
+	name   string
+	salary int
+}
+
+var employees = []Employee{
+	{name: "John", salary: 1000},
+	{name: "Jane", salary: 2000},
+	{name: "Jim", salary: 3000},
+}
+
+func EmployeeIterator(e []Employee) func(func(int, Employee) bool) {
+	return func(yield func(int, Employee) bool) {
+		for i := 0; i < len(e); i++ {
+			if !yield(i, e[i]) {
+				return
+			}
+		}
+	}
+}
+
 func main() {
 	for x := range CountDown(5) {
 		fmt.Println(x)
+	}
+
+	for x, e := range EmployeeIterator(employees) {
+		fmt.Println(x, e)
 	}
 }
