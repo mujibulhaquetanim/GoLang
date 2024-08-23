8cfb46d690ca225869195ff0559529612441cafc
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-08-23 23:51:42 +0600

    simplified complex example of struct using iter.seq2

--- gobasic/iterators/usingITER/usingITER_main.go
+++ gobasic/iterators/usingITER/usingITER_main.go
@@ -16,8 +16,34 @@ func CountDown(v int) iter.Seq[int] {
 	}
 }
 
+// iter.seq2 example:
+type Employee struct {
+	name   string
+	salary int
+}
+
+var employees = []Employee{
+	{name: "John", salary: 1000},
+	{name: "Jane", salary: 2000},
+	{name: "Jim", salary: 3000},
+}
+
+func EmployeeIterator(e []Employee) iter.Seq2[int, Employee] {
+	return func(yield func(int, Employee) bool) {
+		for i := 0; i < len(e); i++ {
+			if !yield(i, e[i]) {
+				return
+			}
+		}
+	}
+}
+
 func main() {
 	for x := range CountDown(5) {
 		fmt.Println(x)
+	}
+
+	for x, e := range EmployeeIterator(employees) {
+		fmt.Println(x, e)
 	}
 }
