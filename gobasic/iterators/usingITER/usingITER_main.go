d5ec71c3de0c891bb940b6a38d6723c9043c00f9
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-08-23 23:42:45 +0600

    simplified iter signature syntax using new iter pkg in v1.23

--- gobasic/iterators/usingITER/usingITER_main.go
+++ gobasic/iterators/usingITER/usingITER_main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"iter"
+)
 
+// iter.seq used instead of func(func(int) bool) syntax
+func CountDown(v int) iter.Seq[int] {
+	return func(yield func(int) bool) {
+		for i := v; i > 0; i-- {
+			if !yield(i) {
+				return
+			}
+		}
+	}
+}
+
+func main() {
+	for x := range CountDown(5) {
+		fmt.Println(x)
+	}
 }
