bb38d20cf0a9f8afdc70be776c74b395eb061fcd
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-08-24 23:30:05 +0600

    backward method of slices added [v1.23]

--- gobasic/iterators/slices/keyFuncs_slicesITER.go
+++ gobasic/iterators/slices/keyFuncs_slicesITER.go
@@ -5,14 +5,17 @@ import (
 	"slices"
 )
 
-// All returns an iterator over index-value pairs in the slice
-// in the usual order.
-
 func main() {
 
 	slice := []int{1, 2, 3}
-
+	// All returns an iterator over index-value pairs in the slice
+	// in the usual order.
 	for x, y := range slices.All(slice) {
-		fmt.Println(x, y)
+		fmt.Println("Returning key-value pairs: ", x, y)
+	}
+	// Backward returns an iterator over index-value pairs in the slice,
+	// traversing it backward with descending indices.
+	for x, y := range slices.Backward(slice) {
+		fmt.Println("Returning backward key-value pairs: ", x, y)
 	}
 }
