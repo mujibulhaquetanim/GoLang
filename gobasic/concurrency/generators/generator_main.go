19186f830bd2812d799b73b8ee34c260959263b6
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-08-25 23:12:25 +0600

    infiniteIntGen created with math/rand, generics and channel

--- gobasic/concurrency/generators/generator_main.go
+++ gobasic/concurrency/generators/generator_main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
+//higerOrderFunc, return a chan of type of passed func.
+func RepeatFunc[T any, K any](done <-chan K, fn func() T) chan T {
+	stream := make(chan T)
+	go func() {
+		defer close(stream)
+		//infinite loop
+		for {
+			select {
+			case <-done:
+				return
+				//accessing stream via closures
+			case stream <- fn():
+			}
+		}
+	}()
+
+	//the goRoutine infinitely shooting fn() data to the stream which is in outside of the goRoutine yet can doing so by closure.
+	return stream
+}
 
 func main() {
-	fmt.Println("generator example")
+	done := make(chan int)
+	defer close(done)
+
+	RandNumFetcher := func() int {
+		return rand.Intn(50000)
+	}
+	for randomValue := range RepeatFunc(done, RandNumFetcher) {
+		fmt.Println(randomValue)
+	}
 }
