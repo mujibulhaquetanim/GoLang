6eb1e40c9a74e9e56bb847866c4ecf61911b7462
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-08-26 23:17:24 +0600

    chunked and sortedFunc added.

--- gobasic/iterators/slices/keyFuncs_slicesITER.go
+++ gobasic/iterators/slices/keyFuncs_slicesITER.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"cmp"
 	"fmt"
 	"slices"
 )
 
+type Person struct {
+	Name string
+	Age  int
+}
+
 func main() {
+	s1 := []Person{{"Emma", 34}, {"Millie", 20}}
+	compare := func(p1, p2 Person) int {
+		return cmp.Compare(p1.Age, p2.Age)
+	}
 
 	slice := []int{1, 2, 3}
+	chunked := slices.Chunk(slice, 2)
 	// All returns an iterator over index-value pairs in the slice
 	// in the usual order.
 	for x, y := range slices.All(slice) {
@@ -35,4 +46,13 @@ func main() {
 	//Sorted collects values from an iterator into new slice then sorts the slice
 	sortedSlice := slices.Sorted(slices.Values(slice))
 	fmt.Println("sortedSlice: ", sortedSlice)
+
+	//SortedFunc is like Sorted but takes a comparison function
+	sortedFunc := slices.SortedFunc(slices.Values(s1), compare)
+	fmt.Println("sortedFunc: ", sortedFunc)
+
+	//chunk returns a iterator over consecutive sub-slices of up to n elements of the slice
+	for x := range chunked {
+		fmt.Println("chunk: ", x)
+	}
 }
