d5d7f33e4db4e4d8e950c344adff22849581427b
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-08-27 23:12:27 +0600

    simplified using iter. [generics]

--- gobasic/iterators/usingGenerics/Generics_iterator_main.go
+++ gobasic/iterators/usingGenerics/Generics_iterator_main.go
@@ -3,7 +3,19 @@ package main
 import (
 	"fmt"
 	"time"
+	"iter"
 )
+
+type Employee struct {
+	Name   string
+	Salary int
+}
+
+// create a pre-defined list of employees  
+var Employees = []Employee{
+	{Name: "Elliot", Salary: 4},
+	{Name: "Donna", Salary: 5},
+}
 
 	func Iterate[E any](e []E) func(func(int, E) bool) {
 		return func(yield func(int, E) bool) {
@@ -18,11 +30,25 @@ import (
 			}
 		}
 	}
+	func EmployeeIterate[E any](e []E) iter.Seq2[int, E] {
+		return func(yield func(int, E) bool) {
+			for i := 0; i <= len(e)-1; i++ {
+				time.Sleep(5 * time.Second)
+				if !yield(i, e[i]) {
+					return
+				}
+			}
+		}
+	}
 
 func main() {
 
 	for x := range Iterate([]int{1, 2, 3}) {
 		fmt.Println(x)
+	}
+
+	for x, e := range EmployeeIterate(Employees) {
+		fmt.Println(x, e)
 	}
 
 }
