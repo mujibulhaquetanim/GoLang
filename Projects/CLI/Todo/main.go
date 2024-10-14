632d10fd8075972d290eddf59b76ae51e03403d0
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-10-14 23:34:59 +0600

    mod file created & some dummy todos added in the main file to test the methods of the todo slice type

--- Projects/CLI/Todo/main.go
+++ Projects/CLI/Todo/main.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
-func main(){
-	fmt.Println("Welcome to Todo CLI")
+func main() {
+	todos := Todos{}
+
+	todos.add("Buy milk")
+	todos.add("Buy eggs")
+	fmt.Printf("%+v\n\n", todos)
+
+	todos.delete(0)
+	fmt.Printf("%+v", todos)
 }
