6f74f81d40bd897cab15ce6ccf3f03e4f705b097
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-10-15 23:43:12 +0600

    removed previous fmt.print & marked first todo as completed & prints the todo using print method

--- Projects/CLI/Todo/main.go
+++ Projects/CLI/Todo/main.go
@@ -1,14 +1,12 @@
 package main
 
-import "fmt"
-
 func main() {
 	todos := Todos{}
 
 	todos.add("Buy milk")
 	todos.add("Buy eggs")
-	fmt.Printf("%+v\n\n", todos)
 
-	todos.delete(0)
-	fmt.Printf("%+v", todos)
+	//toggles the todo at index 0 which will set it to completed
+	todos.toggle(0)
+	todos.print()
 }
