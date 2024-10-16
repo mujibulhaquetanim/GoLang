9f71727e1cc37f3cf2617245fc3aa85140f1426c
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-10-16 23:07:38 +0600

    storage created using newStorage constructor with fileName, which generated new json file of that name, used Save method too

--- Projects/CLI/Todo/main.go
+++ Projects/CLI/Todo/main.go
@@ -2,11 +2,17 @@ package main
 
 func main() {
 	todos := Todos{}
+	//type Todos passed as a generic type to NewStorage & fileName given as string
+	storage := NewStorage[Todos]("todos.json")
+	//passed a pointer of type Todos to Load
+	storage.Load(&todos)
 
-	todos.add("Buy milk")
-	todos.add("Buy eggs")
+	// todos.add("Buy milk")
+	// todos.add("Buy eggs")
 
 	//toggles the todo at index 0 which will set it to completed
-	todos.toggle(0)
+	// todos.toggle(0)
 	todos.print()
+
+	storage.Save(todos)
 }
