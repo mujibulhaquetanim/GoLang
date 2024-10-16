ca705dfc76bffd20bfbed3f6bfc7e3b0cc2b90aa
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-10-16 22:32:00 +0600

    save method added for saving todo data in json format (binary)

--- Projects/CLI/Todo/storage.go
+++ Projects/CLI/Todo/storage.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"encoding/json"
+	"os"
+)
+
 type Storage[T any] struct {
 	FileName string
 }
@@ -9,4 +14,15 @@ func NewStorage[T any](fileName string) *Storage[T] {
 	return &Storage[T]{
 		FileName: fileName,
 	}
+}
+
+func (s *Storage[T]) save(data T) error {
+	//convert data to json, "\t" for indentation & "" used for prefix for each line.
+	fileData, err := json.MarshalIndent(data, "", "\t")
+
+	if err != nil {
+		return err
+	}
+	//0644 = read and write permission for owner only & read permission for others
+	return os.WriteFile(s.FileName, fileData, 0644)
 }
