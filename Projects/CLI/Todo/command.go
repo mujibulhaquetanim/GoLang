883a02224b8a6e79c8bfe122a5a17625b62e92d3
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-10-17 23:58:34 +0600

    execute method added which checks with cmd command with switch case & forward to the respective methods

--- Projects/CLI/Todo/command.go
+++ Projects/CLI/Todo/command.go
@@ -1,6 +1,12 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 type CmdFlags struct {
 	Add    string
@@ -22,4 +28,37 @@ func NewCmdFlags() *CmdFlags {
 	//flag.Parse() is used to parse command line arguments passed to the program from terminal and store them in a struct variable.
 	flag.Parse()
 	return &cf
+}
+
+func (cf *CmdFlags) Execute(todos *Todos) {
+	switch {
+	case cf.List:
+		todos.print()
+
+	case cf.Add != "":
+		todos.add(cf.Add)
+
+	case cf.Edit != "":
+		parts := strings.SplitN(cf.Edit, ":", 2)
+		if len(parts) != 2 {
+			fmt.Println("Error, Invalid for Editing. Please use id: new_title")
+			os.Exit(1)
+		}
+
+		index, err := strconv.Atoi(parts[0])
+		if err != nil {
+			fmt.Println("Error: Invalid index for edit")
+			os.Exit(1)
+		}
+		todos.edit(index, parts[1])
+
+	case cf.Toggle != -1:
+		todos.toggle(cf.Toggle)
+
+	case cf.Delete != -1:
+		todos.delete(cf.Delete)
+
+	default:
+		fmt.Println("Invalid Command")
+	}
 }
