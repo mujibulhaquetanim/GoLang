f5509ad7144e642338b940cea6783c0368e147cd
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-10-18 23:47:41 +0600

    made all the cli commands to lowercase for consistency+convenience

--- Projects/CLI/Todo/command.go
+++ Projects/CLI/Todo/command.go
@@ -18,14 +18,13 @@ type CmdFlags struct {
 
 func NewCmdFlags() *CmdFlags {
 	cf := CmdFlags{}
-
+	
 	flag.StringVar(&cf.Add, "add", "", "Add a new Todo specify title")
-	flag.StringVar(&cf.Edit, "Edit", "", "Edit a new Todo by index & specify a new title. id: new_title")
-	flag.IntVar(&cf.Delete, "Delete", -1, "Specify a todo by index to delete")
-	flag.IntVar(&cf.Toggle, "Toggle", -1, "Specify a todo by index to toggle")
-	flag.BoolVar(&cf.List, "List", false, "List All todos")
+	flag.StringVar(&cf.Edit, "edit", "", "Edit a new Todo by index & specify a new title. id: new_title")
+	flag.IntVar(&cf.Delete, "delete", -1, "Specify a todo by index to delete")
+	flag.IntVar(&cf.Toggle, "toggle", -1, "Specify a todo by index to toggle")
+	flag.BoolVar(&cf.List, "list", false, "List All todos")
 
-	//flag.Parse() is used to parse command line arguments passed to the program from terminal and store them in a struct variable.
 	flag.Parse()
 	return &cf
 }
