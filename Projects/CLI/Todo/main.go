07c47ab26a7b22352f93cbaf4067c3fd63976beb
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-10-19 13:07:00 +0600

    some more infos added in the main file & json storage updated with new/modified todos

--- Projects/CLI/Todo/main.go
+++ Projects/CLI/Todo/main.go
@@ -1,21 +1,23 @@
 package main
 
+import "fmt"
+
 func main() {
+	// Greeting message
+	fmt.Printf("\n ✨ Welcome to Todo CLI Application! ✨\n\n")
 	todos := Todos{}
-	//type Todos passed as a generic type to NewStorage & fileName given as string
-	storage := NewStorage[Todos]("todos.json")
-	//passed a pointer of type Todos to Load
-	storage.Load(&todos)
+	storage := NewStorage[Todos]("todos.json") // created a new instance of Storage wtih fileName & generic type
+	storage.Load(&todos) // passed a pointer of type Todos to the Load function
 
-	cmdFlags := NewCmdFlags()
-	cmdFlags.Execute(&todos)
+	cmdFlags := NewCmdFlags() // created a new instance of CmdFlags
+	cmdFlags.Execute(&todos) // passed a pointer of type Todos to the Execute function
+	storage.Save(todos) // passed a value of type Todos to the Save function
 
+	fmt.Println("To use other commands, please type `./todo-cli -help`")
+	fmt.Printf("\n")
+	fmt.Println("For more information, visit: https://github.com/mujibulhaquetanim/GoLang/tree/main/Projects/CLI/Todo")
+	fmt.Print("\nPress **Enter** if you want to exit...")
 
-	storage.Save(todos)
+	// Prevent the terminal from closing immediately
+	fmt.Scanln()
 }
-
-// todos.print()
-	// todos.add("Buy milk")
-	// todos.add("Buy eggs")
-	//toggles the todo at index 0 which will set it to completed
-	// todos.toggle(0)
