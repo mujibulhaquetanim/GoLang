a8448345cc234f1f280745744cb852961e612863
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-10-19 23:10:08 +0600

    binary file + json storage file updated & main file polished

--- Projects/CLI/Todo/main.go
+++ Projects/CLI/Todo/main.go
@@ -7,15 +7,13 @@ func main() {
 	fmt.Printf("\n ✨ Welcome to Todo CLI Application! ✨\n\n")
 	todos := Todos{}
 	storage := NewStorage[Todos]("todos.json") // created a new instance of Storage wtih fileName & generic type
-	storage.Load(&todos) // passed a pointer of type Todos to the Load function
+	storage.Load(&todos)                       // passed a pointer of type Todos to the Load function
 
 	cmdFlags := NewCmdFlags() // created a new instance of CmdFlags
-	cmdFlags.Execute(&todos) // passed a pointer of type Todos to the Execute function
-	storage.Save(todos) // passed a value of type Todos to the Save function
+	cmdFlags.Execute(&todos)  // passed a pointer of type Todos to the Execute function
+	storage.Save(todos)       // passed a value of type Todos to the Save function
 
-	fmt.Println("To use other commands, please type `./todo-cli -help`")
-	fmt.Printf("\n")
-	fmt.Println("For more information, visit: https://github.com/mujibulhaquetanim/GoLang/tree/main/Projects/CLI/Todo")
+	fmt.Println("\nFor detailed information/help, visit: https://github.com/mujibulhaquetanim/GoLang/tree/main/Projects/CLI/Todo")
 	fmt.Print("\nPress **Enter** if you want to exit...")
 
 	// Prevent the terminal from closing immediately
